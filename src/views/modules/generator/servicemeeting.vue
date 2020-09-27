ed2b0cb27876b5414280464ac69e6a46c183c79f
kyriefang <2470781699@qq.com>
2020-09-27 16:18:32 +0800

    预约记录界面第三次修改

--- src/views/modules/generator/servicemeeting.vue
+++ src/views/modules/generator/servicemeeting.vue
@@ -1,8 +1,6 @@
 <template>
-
 <!-- fang's servicemeeting.vue
-2020-9-27 16:08 -->
-
+2020-9-27 15:50 -->
   <div class="mod-config">
     <el-form :inline="true" :model="dataForm" @keyup.enter.native="getDataList()">
       <el-form-item>
@@ -39,20 +37,29 @@
         prop="department"
         header-align="center"
         align="center"
-        label="使用单位（例:软件学院）">
-      </el-table-column>
-      <el-table-column
-        prop="roomUser"
-        header-align="center"
-        align="center"
-        label="会议室预约人">
+        label="使用单位">
       </el-table-column>
       <el-table-column
         prop="roomName"
         header-align="center"
         align="center"
-        label="会议室名称">
+        label="会议室"
+        width="180">
       </el-table-column>
+      <el-table-column
+        prop="roomUser"
+        header-align="center"
+        align="center"
+        label="预约人"
+        width="70">
+      </el-table-column>
+      <!-- <el-table-column
+        prop="roomDate"
+        header-align="center"
+        align="center"
+        label="会议日期"
+        width="100">
+      </el-table-column> -->
       <el-table-column
         prop="startTime"
         header-align="center"
@@ -68,7 +75,7 @@
         width="102">
       </el-table-column>
       <el-table-column
-        prop="headCount"
+        prop="meetingTheme"
         header-align="center"
         align="center"
         label="会议主题"
@@ -88,28 +95,29 @@
         label="参会人数">
       </el-table-column>
       <el-table-column
-        prop="meetingTheme"
+        prop="equipment"
         header-align="center"
         align="center"
-        label="会议主题">
+        label="会场准备">
       </el-table-column>
       <el-table-column
         prop="remark"
         header-align="center"
         align="center"
-        label="备注">
+        label="备注"
+        width="50">
       </el-table-column>
       <el-table-column
         prop="status"
         header-align="center"
         align="center"
-        label="预约状态（0:审核中 1，通过，2：未通过，3:已取消）">
+        label="预约状态">
       </el-table-column>
       <el-table-column
         fixed="right"
         header-align="center"
         align="center"
-        width="150"
+        width="50"
         label="操作">
         <template slot-scope="scope">
           <!-- <el-button type="text" size="small" @click="addOrUpdateHandle(scope.row.orderId)">修改</el-button> -->
@@ -117,6 +125,7 @@
         </template>
       </el-table-column>
     </el-table>
+    <!-- 分页处理 -->
     <el-pagination
       @size-change="sizeChangeHandle"
       @current-change="currentChangeHandle"
