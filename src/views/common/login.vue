bf4f3077437f33a5f0844e465d1da5b24485522c
kyriefang <2470781699@qq.com>
2020-10-07 09:50:52 +0800

    开发者的首页改成了ahome.vue，所有的title和登录界面改成了“会议预约系统”

--- src/views/common/login.vue
+++ src/views/common/login.vue
@@ -3,8 +3,8 @@
     <div class="site-content__wrapper">
       <div class="site-content">
         <div class="brand-info">
-          <h2 class="brand-info__text">renren-fast-vue</h2>
-          <p class="brand-info__intro">renren-fast-vue基于vue、element-ui构建开发，实现renren-fast后台管理前端功能，提供一套更优的前端解决方案。</p>
+          <h2 class="brand-info__text">会议预约系统</h2>
+          <p class="brand-info__intro">会议预约系统为大连理工大学的教师提供方便快捷的会议预约服务，一约即。</p>
         </div>
         <div class="login-main">
           <h3 class="login-title">管理员登录</h3>
@@ -81,7 +81,12 @@
             }).then(({data}) => {
               if (data && data.code === 0) {
                 this.$cookie.set('token', data.token)
-                this.$router.replace({ name: 'home' })
+                if(this.dataForm.userName=="root"){
+                  this.$router.replace({ name: 'ahome' })
+                }else{
+                  this.$router.replace({ name: 'home' })
+                }
+                
               } else {
                 this.getCaptcha()
                 this.$message.error(data.msg)
